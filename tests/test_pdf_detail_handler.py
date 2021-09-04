9b8b0d1b7750526a296613f5fd50729cd0759fe7
Wilson <wilsontads@gmail.com>
2021-09-04 16:49:58 -0300

    # add get pdf detail fail test

--- tests/test_pdf_detail_handler.py
+++ tests/test_pdf_detail_handler.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 def test_get_detail_success(pdf_detail_handler, valid_contract):
     returned_value = pdf_detail_handler.get_detail(
         f"{valid_contract['folder']}/{valid_contract['file_name']}"
@@ -8,3 +11,10 @@ def test_get_detail_success(pdf_detail_handler, valid_contract):
         "date": "13/04/2021",
         "write_in_days": 100,
     }
+
+
+def test_get_detail_file_not_found(pdf_detail_handler, valid_contract):
+    with pytest.raises(FileNotFoundError) as exc:
+        pdf_detail_handler.get_detail(f"{valid_contract['folder']}/fake.pdf")
+    assert exc.type == FileNotFoundError
+    assert "No such file" in str(exc)
