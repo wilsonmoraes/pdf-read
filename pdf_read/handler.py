b67c3a5cca50ad31433f6d0e6301bd41944469e1
Wilson <wilsontads@gmail.com>
2021-09-03 22:46:47 -0300

    # add lint # created initial pdf detail functions

--- pdf_read/handler.py
+++ pdf_read/handler.py
@@ -1,48 +1,43 @@
 import glob
 import re
 
-import PyPDF2
+from config import settings
 from pdfminer.high_level import extract_text
 
-from config import settings
-from price_parser import Price
 
-regex_currency = re.compile(
-    r'\$?(?:(?:[1-9][0-9]{0,2})(?:,[0-9]{3})+|[1-9][0-9]*|0)(?:[\.,][0-9][0-9]?)?(?![0-9]+)'
-)
+class ExtractPdfInfoHandler:
+    def get_all_contract_details(self):
+        pdfs = glob.glob(f"{settings.READ_PDFS_FROM_FOLDER}/*.pdf")
+        for pdf in pdfs:
+            return [s for s in self.get_contract_detail(pdf)]
 
+    def get_contract_detail(self, pdf_path):
+        content = extract_text(pdf_path)
+        content = [s for s in content.splitlines() if s]
+        return {
+            "contract_number": self.get_contract_number(content),
+            "amount": self.get_amount(content),
+            "write_in_days": self.get_draw_up_in_days(content),
+            "date": self.get_contract_date(content),
+        }
 
-def pdf_to_str_PyPDF2():
-    content = ""
+    def get_amount(self, content: [str]):
+        amount = list(filter(lambda x: x.startswith("3.1."), content))[0]
+        return re.findall(
+            r"""
+                ([.]?\d[\d\s.,]*)   # number, probably with thousand separators
+                \s*?                # skip whitespace
+                """,
+            amount,
+            re.VERBOSE,
+        )[1]
 
-    pdfs = glob.glob(f"{settings.READ_PDFS_FROM_FOLDER}/*.pdf")
-    for pdf in pdfs:
-        pdf_file = open(pdf, 'rb')
-        read_pdf = PyPDF2.PdfFileReader(pdf_file)
-        num_pages = read_pdf.getNumPages()
-        for i in range(0, num_pages):
-            content += read_pdf.getPage(i).extractText() + "\n"
-            content = " ".join(content.replace(u"\xa0", " ").strip().split())
+    def get_draw_up_in_days(self, content: [str]):
+        write_in_days = next(filter(lambda x: x.startswith("A Escritura deverá ser lavrada"), content))
+        return int(re.search(r"\d+", write_in_days).group())
 
-        print(f"file={pdf[:-4]},text={content}")
-        break
+    def get_contract_number(self, content: [str]):
+        return content[3].strip()
 
-
-def pdf_to_str_pdfminer():
-    content = ""
-
-    pdfs = glob.glob(f"{settings.READ_PDFS_FROM_FOLDER}/*.pdf")
-    for pdf in pdfs:
-        content += extract_text(pdf)
-        filtered = [s for s in content.splitlines() if s]
-        contract_number = filtered[3].strip()
-        amount = list(filter(lambda x: x.startswith("3.1."), filtered))[0]
-        amount = Price.fromstring(amount)
-        write_in_days = list(filter(lambda x: x.startswith("A Escritura deverá ser lavrada"), filtered))[0]
-        write_in_days = int(re.search(r'\d+', write_in_days).group())
-        date = re.search(r"([0-9]{2}\/[0-9]{2}\/[0-9]{4})", filtered[-1]).group()
-        print(f"file={pdf[:-4]},text={content}")
-        break
-
-
-pdf_to_str_pdfminer()
+    def get_contract_date(self, content: [str]):
+        return re.search(r"([0-9]{2}\/[0-9]{2}\/[0-9]{4})", content[-1]).group()
